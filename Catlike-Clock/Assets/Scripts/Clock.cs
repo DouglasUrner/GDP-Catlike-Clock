73e85d42b455ea2bf1751f81b58e8981ae7f06fe
Douglas Urner <dlu@canishe.com>
2018-05-02 22:22:13 -0700

    2.1 - Empty class.

--- Catlike-Clock/Assets/Scripts/Clock.cs
+++ Catlike-Clock/Assets/Scripts/Clock.cs
@@ -1,16 +1,1 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Clock : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-}
+﻿public class Clock {}
