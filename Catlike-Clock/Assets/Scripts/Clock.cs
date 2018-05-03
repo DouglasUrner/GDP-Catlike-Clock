3ca42e3d0f896b5218e74a6fee9640b7491aa746
Douglas Urner <dlu@canishe.com>
2018-05-02 23:22:21 -0700

    Awake with DateTime.Now.

--- Catlike-Clock/Assets/Scripts/Clock.cs
+++ Catlike-Clock/Assets/Scripts/Clock.cs
@@ -1,10 +1,11 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class Clock : MonoBehaviour {
 	// Access to clock hands.
 	public Transform hourTransform, minuteTransform, secondTransform;
 
 	void Awake() {
-		Debug.Log(Time.time);
+		Debug.Log(DateTime.Now);
 	}
 }
